6b0a0606b9b4c2ce89a0dfec476a9c5bcb9e9be6
weny <weny@annyandandy.com>
2018-04-02 14:58:20 +0800

    源文件

--- src/index.js
+++ src/index.js
@@ -1,46 +1,53 @@
-import '../style/common.css';
-import '../style/detials.css';
+import './style/common.css';
+import './style/detials.css';
 let str = '';
             
 const  data = [
         [
             { title : 'list'},
-            { url : 'e/1/index.html', title : '去除字符串' },
-            { url : 'e/2/index.html', title : '验证用户' },
-            { url : 'e/3/index.html', title : '禁止用户输入' },
-            { url : 'e/4/index.html', title : '关闭用户中' },
+            { url : 'list/timer/index.html', title : '简单计数器--redux' },
+            { url : 'list/dialog/index.html', title : '对话框--面向对象' },
+
+           
             
         ],
         [
             { title : 'image'},
-            { url : 'e/16/index.html', title : '可添加的下拉选项' },
-            { url : 'e/17/index.html', title : '图片放大镜效果' },
-            { url : 'e/18/index.html', title : '点击图片逐渐放大' }
-        ],
-        [
-            { title : 'content'},
-            { url : 'e/19/index.html', title : '单元行上鼠标悬停提示' },
-            { url : 'c/6/index.html', title : '无刷新切换皮肤' },
-            { url : 'e/20/index.html', title : '隔行换色鼠标经过换色' },
-            { url : 'e/21/index.html', title : '添加或删除表格行' },
+            { url : 'image/index1.html', title : '图片罩层' }
             
         ],
         [
+            { title : 'content'},
+            { url : 'content/tab/index.html', title : 'tab切换' },
+            { url : 'content/mouseover/index.html', title : '鼠标移入显示移出隐藏' },
+
+            
+            
+        ],
+        [
+            {title:'css3'},
+            { url : 'css3/loadingAnimation.html', title : 'css3实现loading动画' },
+
+        ],
+        [
             { title : 'carousel'},
-            { url : 'slide/01/index.html', title : '百度爱玩 - 图片轮换' },
-            { url : 'slide/02/bd02.html', title : '百度爱玩 - 遮罩' },
-            { url : 'slide/03/bd03.html', title : '百度爱玩 - 榜单tab切换' },
+            { url : 'carousel/seamless/index.html', title : '无缝滚动' },
+            { url : 'carousel/singleslide/index.html', title : '滑动轮播' },
+            { url : 'carousel/singleswitch/index.html', title : '切换轮播' },
+            { url : 'carousel/carousel_3d/index.html', title : '3d轮播' },
+
             
         ],
         
         [
-            { title : 'data'},
-            { url : 'd/03/index.html', title : '全日历' },
-            { url : 'd/04/index.html', title : 'canvas实现' }
+            { title : 'date'},
+            { url : 'date/simple_calendar/index.html', title : '简单日历' },
+            { url : 'date/calendar/index.html', title : '全日历' }
         ],
         [
-            { title : 'todoList'},
-            { url : 'product/todolist/index.html', title : '乘法口诀输出' }
+            { title : 'other'},
+            { url : 'something/todolist/index.html', title : 'todoList--react' },
+            {url:'something/totop/index.html',title:'回到顶部按钮'}
         ],
         [
             {title:'waiting...'}
