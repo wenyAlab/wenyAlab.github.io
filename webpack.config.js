9854d00fc2de338dd91c7e66bfe375dd1d30db5e
weny <weny@annyandandy.com>
2018-04-02 14:43:21 +0800

    配置文件 webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry:{
+        entry:'./index.js'
+    },
+    output:{
+        filename:'main.js',
+        path:path.resolve(__dirname,'dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                loader:'babel-loader',
+                options:{
+                    presets:['es2015','react']
+                }
+            },
+            {
+                test:/\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    devServer:{
+        contentBase:'./',
+        inline:true,
+        port:7000
+    }
+}
